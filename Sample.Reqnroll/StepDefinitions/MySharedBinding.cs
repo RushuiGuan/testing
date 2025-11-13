41a7b396ebe4b86edc3fefc50901eef9c5d7eb1c
rushuiguan <rushui@gmail.com>
2025-11-13 00:37:19 -0500

    bump version to 10 to align with current dotnet version.  moving away from reqnroll

--- Sample.Reqnroll/StepDefinitions/MySharedBinding.cs
+++
@@ -1,9 +0,0 @@
-﻿using Albatross.Reqnroll;
-
-namespace Sample.Reqnroll.StepDefinitions {
-	[Binding]
-	public class MySharedBinding : SharedBindings {
-		public MySharedBinding(ScenarioContext scenario) : base(scenario) {
-		}
-	}
-}
