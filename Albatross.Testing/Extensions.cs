9712ba54dce6d85f951f94db26a6e2e3f936b9f5
rushuiguan <rushui@outlook.com>
2025-12-03 12:27:28 -0500

    update EqualsIgnoringLineEndings

--- Albatross.Testing/Extensions.cs
+++ Albatross.Testing/Extensions.cs
@@ -14,14 +14,23 @@ namespace Albatross.Testing {
 		/// </summary>
 		/// <param name="text"></param>
 		/// <returns></returns>
-		public static string NormalizeLineEnding(this string? text) {
-			if (string.IsNullOrEmpty(text)) return text;
+		public static string? NormalizeLineEnding(this string? text) {
+			if (string.IsNullOrEmpty(text)) { return text; }
 			return text.Replace("\r", "").TrimEnd('\n');
 		}
 
+		/// <summary>
+		/// compare 2 string without carriage return or line feed
+		/// </summary>
+		/// <param name="expected"></param>
+		/// <param name="actual"></param>
 		public static void EqualsIgnoringLineEndings(this string? expected, string? actual) {
-			expected = expected.NormalizeLineEnding();
-			actual = actual.NormalizeLineEnding();
+			if (!string.IsNullOrEmpty(expected)) {
+				expected = expected.Replace("\r", "").Replace("\n", "");
+			}
+			if(!string.IsNullOrEmpty(actual)) {
+				actual = actual.Replace("\r", "").Replace("\n", "");
+			}
 			Assert.Equal(expected, actual);
 		}
 	}
